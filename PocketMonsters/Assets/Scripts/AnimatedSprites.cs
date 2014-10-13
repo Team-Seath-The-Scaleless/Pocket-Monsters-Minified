4c96f2c581c763ab0b98ab8a19588ce6ee583150
Hristo Petrov <hpetrovv@gmail.com>
2014-10-13 18:52:46 +0300

    Added NGUI GUI library, added animation control fields

--- PocketMonsters/Assets/Scripts/AnimatedSprites.cs
+++ PocketMonsters/Assets/Scripts/AnimatedSprites.cs
@@ -1,4 +1,4 @@
-﻿namespace Assets.Scripts
+﻿namespace Scripts
 {
     using System;
     using UnityEngine;
@@ -12,23 +12,27 @@
         public int uvAnimationTileY = 0;
         public float framesPerSecond = 10.0f;
 
-        void Update()
+        public float offsetY = 0.75f;
+        public int rowImages = 3;
+
+        public void Update()
         {
             //Calculate index.
             int index = (int)(Time.time * framesPerSecond);
-
+            
             //Repeat when exhausting all frames.
-            index = index % (3 * 1);
+            index = index % (rowImages * 1);
 
             //Size of every tile.
             Vector2 size = new Vector2((1.0f / uvAnimationTileX), (1.0f / uvAnimationTileY));
 
             //Split into horizontal and vertical index.
             float uIndex = index % uvAnimationTileX;
-            float vIndex = index / uvAnimationTileX;
+            //float vIndex = index / uvAnimationTileX;
 
             //Build offset.
-            Vector2 offset = new Vector2(uIndex * size.x, 1.0f - size.y - vIndex * size.y);
+            //Vector2 offset = new Vector2(uIndex * size.x, 1.0f - size.y - vIndex * size.y);
+            Vector2 offset = new Vector2(uIndex * size.x, offsetY);
 
             renderer.material.SetTextureOffset("_MainTex", offset);
             renderer.material.SetTextureScale("_MainTex", size);
