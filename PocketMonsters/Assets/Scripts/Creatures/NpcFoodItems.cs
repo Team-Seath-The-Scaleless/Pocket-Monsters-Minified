aa929b437717cdc1fa9ea65b3c924f920bc846c9
Ivaylo Gluhchev <jsl@abv.bg>
2014-10-16 14:13:52 +0300

    adding food items

---
+++ PocketMonsters/Assets/Scripts/Creatures/NpcFoodItems.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Common;
+using Creatures;
+
+namespace Assets.Scripts.Creatures
+{
+    class NpcFoodItems
+    {
+    }
+}
---
+++ PocketMonsters/Assets/Scripts/Move/HeroPosition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Interfaces;
+
+namespace Assets.Scripts.Move
+{  
+    public abstract class HeroPosition : IMovable
+    {
+        public int StartPosition { get; set; }
+        public int CurrentPosition { get; set; }
+    }
+}
